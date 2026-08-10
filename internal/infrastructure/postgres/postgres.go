335e998f10eebd138783859e3b955494860de630
nurgal1ev <n.el34@yandex.ru>
2026-08-10 12:53:48 +0300

    feat: добавил авто миграции

--- internal/infrastructure/postgres/postgres.go
+++ internal/infrastructure/postgres/postgres.go
@@ -1,6 +1,8 @@
 package postgres
 
 import (
+	"log"
+
 	"github.com/nurgal1ev/booking-service/internal/config"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -14,6 +16,12 @@ func NewDb(c *config.Config) *Db {
 	db, err := gorm.Open(postgres.Open(c.DbConfig.DSN), &gorm.Config{})
 	if err != nil {
 		panic(err)
+	}
+
+	err = db.AutoMigrate()
+	if err != nil {
+		log.Printf("Error during migration: %v", err)
+		return nil
 	}
 
 	return &Db{db}
