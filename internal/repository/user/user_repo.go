7aeabd4588933fdb66b141bc1b64f39537bde5ab
nurgal1ev <n.el34@yandex.ru>
2026-08-14 12:44:29 +0300

    feat: добавил слой репозитория user_repo.go

--- internal/repository/user/user_repo.go
+++ internal/repository/user/user_repo.go
@@ -1,1 +1,59 @@
 package user
+
+import (
+	"context"
+	"errors"
+
+	"github.com/nurgal1ev/booking-service/internal/models"
+	"gorm.io/gorm"
+)
+
+type UserRepo struct {
+	db *gorm.DB
+}
+
+func NewUserRepo(db *gorm.DB) *UserRepo {
+	return &UserRepo{db: db}
+}
+
+func (u *UserRepo) Create(ctx context.Context, user *models.User) error {
+	query := u.db.WithContext(ctx).Create(user).Error
+	return query
+}
+
+func (u *UserRepo) FindByID(ctx context.Context, id int) (*models.User, error) {
+	var user models.User
+	err := u.db.WithContext(ctx).Where("id = ?", id).First(&user).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (u *UserRepo) FindByEmail(ctx context.Context, email string) (*models.User, error) {
+	var user models.User
+	err := u.db.WithContext(ctx).Where("email = ?", email).First(&user).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (u *UserRepo) GetAll(ctx context.Context) ([]models.User, error) {
+	var users []models.User
+	err := u.db.WithContext(ctx).Find(&users).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return []models.User{}, nil
+		}
+		return nil, err
+	}
+	return users, nil
+}
