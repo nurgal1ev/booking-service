fd3dd223d4d0b9d72bb29b0fed7a57fa0de19003
nurgal1ev <n.el34@yandex.ru>
2026-08-17 10:20:05 +0300

    feat: добавил поле UpdatedAt

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,11 +1,16 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
-	Username string
-	Email    string `gorm:"unique"`
-	Password string
-	Role     string
+	Username  string
+	Email     string `gorm:"unique"`
+	Password  string
+	Role      string
+	UpdatedAt *time.Time
 }
