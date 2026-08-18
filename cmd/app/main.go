a3213367a5b5e318624192b91d1158470f02cd50
nurgal1ev <n.el34@yandex.ru>
2026-08-18 12:45:37 +0300

    feat: добавил зависимости слоев

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -5,12 +5,27 @@ import (
 
 	"github.com/nurgal1ev/booking-service/internal/config"
 	"github.com/nurgal1ev/booking-service/internal/infrastructure/postgres"
+	userRepo "github.com/nurgal1ev/booking-service/internal/repository/user"
+	userService "github.com/nurgal1ev/booking-service/internal/service/user"
+	httpv1 "github.com/nurgal1ev/booking-service/internal/transport/httpv1"
+	userHandler "github.com/nurgal1ev/booking-service/internal/transport/httpv1/handler/user"
 )
 
 func main() {
 	cfg := config.Load()
-	db := postgres.NewDb(cfg)
-	if db == nil {
-		log.Fatal("Failed to initialize database")
+
+	db, err := postgres.NewDb(cfg)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	userRepository := userRepo.NewUserRepo(db.DB)
+	userService := userService.NewUserService(userRepository)
+	userHandler := userHandler.NewUserHandler(userService)
+
+	handlers := httpv1.Handlers{
+		User: userHandler,
+	}
+
+	httpv1.StartServer(handlers)
 }
