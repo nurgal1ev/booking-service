39bacdcf764a17667c6865c862b77b94ce62aaa5
nurgal1ev <n.el34@yandex.ru>
2026-09-07 11:16:01 +0300

    feat: добавил метод Create в хэндлер

--- internal/transport/httpv1/handler/property/dto.go
+++ internal/transport/httpv1/handler/property/dto.go
@@ -1,1 +1,22 @@
 package property
+
+type CreatePropertyInput struct {
+	Name          string `json:"name"`
+	Description   string `json:"description"`
+	Address       string `json:"address"`
+	City          string `json:"city"`
+	Country       string `json:"country"`
+	PricePerNight int    `json:"price_per_night"`
+	PropertyType  string `json:"property_type"`
+}
+
+type CreatePropertyOutput struct {
+	ID            uint   `json:"id"`
+	Name          string `json:"name"`
+	Description   string `json:"description"`
+	Address       string `json:"address"`
+	City          string `json:"city"`
+	Country       string `json:"country"`
+	PricePerNight int    `json:"price_per_night"`
+	PropertyType  string `json:"property_type"`
+}
--- internal/transport/httpv1/handler/property/handler.go
+++ internal/transport/httpv1/handler/property/handler.go
@@ -1,1 +1,65 @@
 package property
+
+import (
+	"context"
+
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/nurgal1ev/booking-service/internal/service/property"
+	"github.com/nurgal1ev/booking-service/internal/transport/middleware"
+)
+
+type PropertyHandler struct {
+	propertyService *property.PropertyService
+}
+
+func NewPropertyHandler(propertyService *property.PropertyService) *PropertyHandler {
+	return &PropertyHandler{
+		propertyService: propertyService,
+	}
+}
+
+func (h *PropertyHandler) CreateProperty(ctx context.Context, input *CreatePropertyInput) (*CreatePropertyOutput, error) {
+	userID, ok := middleware.GetUserIDFromContext(ctx)
+	if !ok || userID == 0 {
+		return nil, huma.Error401Unauthorized("user not authenticated")
+	}
+
+	if input.Name == "" {
+		return nil, huma.Error400BadRequest("name is required")
+	}
+
+	if input.PricePerNight <= 0 {
+		return nil, huma.Error400BadRequest("price per night must be greater than 0")
+	}
+
+	if input.PropertyType == "" {
+		return nil, huma.Error400BadRequest("property type is required")
+	}
+
+	property := &property.Property{
+		Name:          input.Name,
+		Description:   input.Description,
+		Address:       input.Address,
+		City:          input.City,
+		Country:       input.Country,
+		PricePerNight: input.PricePerNight,
+		PropertyType:  input.PropertyType,
+		OwnerID:       userID,
+	}
+
+	result, err := h.propertyService.Create(ctx, property)
+	if err != nil {
+		return nil, err
+	}
+
+	return &CreatePropertyOutput{
+		ID:            result.ID,
+		Name:          result.Name,
+		Description:   result.Description,
+		Address:       result.Address,
+		City:          result.City,
+		Country:       result.Country,
+		PricePerNight: result.PricePerNight,
+		PropertyType:  result.PropertyType,
+	}, nil
+}
