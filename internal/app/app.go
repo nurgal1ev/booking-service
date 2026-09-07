e0b8de41c1d337584155d95de66f213372b822d6
nurgal1ev <n.el34@yandex.ru>
2026-09-07 14:45:10 +0300

    feat: добавил зависмости

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,9 +2,12 @@ package app
 
 import (
 	"github.com/nurgal1ev/booking-service/internal/config"
+	"github.com/nurgal1ev/booking-service/internal/repository/property"
 	userRepo "github.com/nurgal1ev/booking-service/internal/repository/user"
+	propertyService "github.com/nurgal1ev/booking-service/internal/service/property"
 	userService "github.com/nurgal1ev/booking-service/internal/service/user"
 	"github.com/nurgal1ev/booking-service/internal/transport/httpv1"
+	propertyHandler "github.com/nurgal1ev/booking-service/internal/transport/httpv1/handler/property"
 	userHandler "github.com/nurgal1ev/booking-service/internal/transport/httpv1/handler/user"
 	"gorm.io/gorm"
 )
@@ -14,7 +17,12 @@ func InitHandlers(db *gorm.DB, cfg *config.Config) httpv1.Handlers {
 	userService := userService.NewUserService(userRepository, cfg.Auth.Secret)
 	userHandler := userHandler.NewUserHandler(userService)
 
+	propertyRepository := property.NewPropertyRepo(db)
+	propertyService := propertyService.NewPropertyService(propertyRepository, userRepository)
+	propertyHandler := propertyHandler.NewPropertyHandler(propertyService)
+
 	return httpv1.Handlers{
-		User: userHandler,
+		User:     userHandler,
+		Property: propertyHandler,
 	}
 }
